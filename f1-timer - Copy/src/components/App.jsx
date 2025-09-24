cc504d873e21aa32b44907fde196ec1b00695746
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-09-24 21:59:39 +0800

    Add files via upload

---
+++ f1-timer - Copy/src/components/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import Lights from './Lights'
+import Nav from './Nav'
+
+export default function App() {
+  const [startLights, setStartLights] = useState(false)
+  const [runTimer, setRunTimer] = useState(false)
+
+  // spacebar press handler
+  useEffect(() => {
+    const handleKey = (e) => {
+      if (e.code === "Space") {
+        if (!startLights && !runTimer) {
+          setStartLights(true)  // start lights sequence
+        } else if (runTimer) {
+          setRunTimer(false)   // stop timer
+          setStartLights(false)
+        }
+      }
+    }
+    window.addEventListener("keydown", handleKey)
+    return () => window.removeEventListener("keydown", handleKey)
+  }, [startLights, runTimer])
+
+  return (
+    <>
+      <Lights 
+        startLights={startLights} 
+        runTimer={runTimer}
+        onLightsOut={() => setRunTimer(true)}
+      />
+    </>
+  )
+}
---
+++ f1-timer - Copy/src/components/Lights.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react'
+import '../css/lights.css'
+
+export default function Lights(props) {
+  const [time, setTime] = useState(0)
+  const [currentColumn, setCurrentColumn] = useState(-1)
+  const [bestTime, setBestTime] = useState(0)
+  const COLUMNS = 5
+  const ROWS = 4
+
+  useEffect(() => {
+    let interval
+    if (props.startLights) {
+      setCurrentColumn(-1)
+      let col = -1
+      interval = setInterval(() => {
+        col += 1
+        if (col < COLUMNS) {
+          setCurrentColumn(col)
+        } else {
+          clearInterval(interval)
+          props.onLightsOut?.()
+        }
+      }, 1000)
+    } else {
+      setCurrentColumn(-1)
+    }
+    return () => clearInterval(interval)
+  }, [props.startLights])
+
+  useEffect(() => {
+    let interval
+    if (props.runTimer) {
+      interval = setInterval(() => {
+        setTime((prev) => prev + 10)
+      }, 10)
+    } else {
+        let timer = (time / 1000).toFixed(3)
+        if (timer != bestTime) {
+            setBestTime(timer)
+        } else if (timer < bestTime) {
+            setBestTime(timer)
+        }
+        setTime(0)
+    }
+    return () => clearInterval(interval)
+  }, [props.runTimer])
+
+  const formattedTime = (time / 1000).toFixed(3)
+
+  return (
+    <>
+      <div className="lights-container">
+        {Array.from({ length: COLUMNS }).map((_, stripIndex) => (
+          <div key={stripIndex} className="light-strip">
+            {Array.from({ length: ROWS }).map((_, rowIndex) => {
+              const isBottomTwo = rowIndex >= ROWS - 2
+              const isLit = stripIndex <= currentColumn && isBottomTwo
+              return (
+                <div
+                  key={`${stripIndex}-${rowIndex}`}
+                  className="light"
+                  style={{ backgroundColor: isLit ? 'red' : 'grey' }}
+                />
+              )
+            })}
+          </div>
+        ))}
+      </div>
+
+      <p id="instruction">
+        <u>Tap</u> or hit "<u>space</u>" to start, then <u>tap</u> or hit "<u>space</u>" again when the lights go out.
+      </p>
+      <h1 id="timer">{formattedTime}</h1>
+      <p id="credits">Personal Best: {bestTime}</p>
+      <p id="credits">
+        Created by <a href="https://github.com/Muelvzz">Muelvin Lopez</a>. Based from this{' '}
+        <a href="https://f1-start.jakearchibald.com/">F1 Start Timer</a>
+      </p>
+    </>
+  )
+}
